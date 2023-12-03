c645241ca0adcec974134f2a3a02c393d8fba8fe
MyCool <ron035994@gmail.com>
2023-12-03 10:01:11 +0200

    create the start of grid handle inputs events function (only up)

--- Grid.py
+++ Grid.py
@@ -27,8 +27,29 @@ class Grid(MonoBehaviour):
     def create_random_box(self):
         pass
 
+    def handle_input(self, action: tuple):
+        # i - current row
+        # k - current object in row
+        # z - current row to checking with the k
+
+        if action == (0, 1):
+            for i in range(self.grid_size, -1, -1):
+                for k in range(self.grid_size):
+                    for z in range(self.grid_size):
+                        if self.grid[z][k] is None:
+                            self.grid[z][k] = self.grid[i][k]
+                            del self.grid[z][k]
+                            break
+        if action == (0, -1):
+            pass
+
+
+
     def update(self, dt: float, events: list):
-        pass
+        for event in events:
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_DOWN:
+                    pass
 
     def render(self, surface: pygame.Surface):
         pygame.draw.rect(surface, self.color, pygame.Rect(self.position, self.size))
