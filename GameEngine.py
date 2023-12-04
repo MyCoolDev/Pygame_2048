8db268aef60c91260f6ee4090db3ef59a23a441e
MyCool <ron035994@gmail.com>
2023-12-04 17:19:59 +0200

    Merge!!!

--- GameEngine.py
+++ GameEngine.py
@@ -33,7 +33,14 @@ class GameEngine:
         self.logo_duration = 3
         self.logo_parts_duration_each = (self.logo_duration - 1) / 4
         self.logo_content = 0000
-        self.logo_text = Text("0000", "Poppins", 100, pygame.Vector2(self.screen.get_width() / 2, self.screen.get_height() / 2), (255, 255, 255), alpha=0)
+        self.logo_starting_font_size = 120
+        self.logo_start_pos = pygame.Vector2(self.screen.get_width() / 2, self.screen.get_height() / 2)
+        self.logo_text = Text("0000", "Poppins", self.logo_starting_font_size, self.logo_start_pos, (255, 255, 255), alpha=0)
+        self.logo_shrink_duration = 1.5
+        self.logo_final_font_size = 80
+        self.logo_delta_font_size = self.logo_starting_font_size - self.logo_final_font_size
+        self.logo_end_pos = pygame.Vector2(self.screen.get_width() / 2, 1 * self.screen.get_height() / 5)
+        self.logo_delta_pos = self.logo_start_pos - self.logo_end_pos
 
         self.running = True
         self.clock = pygame.time.Clock()
@@ -60,43 +67,6 @@ class GameEngine:
             self.events = pygame.event.get()
             self.handle_events()
 
-            if self.state == "intro":
-                self.intro_timer += self.dt
-                if self.intro_fade_timer < self.intro_production_fade_sum_duration:
-                    self.intro_fade_timer += self.dt
-
-                if self.intro_timer < self.intro_production_fade_in_duration:
-                    alpha = int((255 / 3) * self.intro_fade_timer)
-                    self.intro_production_text.alpha = alpha
-                    self.intro_production_text.update_alpha()
-                elif 0 < self.intro_timer - self.intro_production_fade_in_duration < self.intro_production_duration:
-                    self.intro_fade_timer = 0
-                elif 0 < self.intro_timer - self.intro_production_fade_in_duration - self.intro_production_duration < self.intro_production_fade_out_duration:
-                    self.intro_fade_timer += self.dt
-                    alpha = int((255 / 3) * (3 - self.intro_fade_timer))
-                    self.intro_production_text.alpha = alpha
-                    self.intro_production_text.update_alpha()
-
-                # logo 2048
-                elif 0 < self.intro_timer - self.intro_production_fade_sum_duration < self.logo_duration:
-                    print("test")
-                    self.logo_text.alpha = 255
-                    self.logo_text.update_alpha()
-
-                    self.logo_content = [f"{random.randint(0, 9)}", f"{random.randint(0, 9)}", f"{random.randint(0, 9)}", f"{random.randint(0, 9)}"]
-                    if self.logo_parts_duration_each <= self.intro_timer - self.intro_production_fade_sum_duration:
-                        self.logo_content[0] = "2"
-                        if 2 * self.logo_parts_duration_each <= self.intro_timer - self.intro_production_fade_sum_duration:
-                            self.logo_content[1] = "0"
-                        if 3 * self.logo_parts_duration_each <= self.intro_timer - self.intro_production_fade_sum_duration:
-                            self.logo_content[2] = "4"
-                        if 3 * self.logo_parts_duration_each <= self.intro_timer - self.intro_production_fade_sum_duration:
-                            self.logo_content[3] = "8"
-
-                    self.logo_text.update_text("".join(self.logo_content))
-            elif self.state == "game":
-                pass
-
             self.update()
             self.render()
             pygame.display.flip()
@@ -111,7 +81,50 @@ class GameEngine:
         self.screen.fill("black")
 
     def update(self):
-        if self.state == "game":
+        if self.state == "intro":
+            self.intro_timer += self.dt
+            if self.intro_fade_timer < self.intro_production_fade_sum_duration:
+                self.intro_fade_timer += self.dt
+
+            if self.intro_timer < self.intro_production_fade_in_duration:
+                alpha = int((255 / 3) * self.intro_fade_timer)
+                self.intro_production_text.alpha = alpha
+                self.intro_production_text.update_alpha()
+            elif 0 < self.intro_timer - self.intro_production_fade_in_duration < self.intro_production_duration:
+                self.intro_fade_timer = 0
+            elif 0 < self.intro_timer - self.intro_production_fade_in_duration - self.intro_production_duration < self.intro_production_fade_out_duration:
+                self.intro_fade_timer += self.dt
+                alpha = int((255 / 3) * (3 - self.intro_fade_timer))
+                self.intro_production_text.alpha = alpha
+                self.intro_production_text.update_alpha()
+
+            # logo 2048
+            elif 0 < self.intro_timer - self.intro_production_fade_sum_duration < self.logo_duration:
+                self.logo_text.alpha = 255
+                self.logo_text.update_alpha()
+
+                self.logo_content = [f"{random.randint(0, 9)}", f"{random.randint(0, 9)}", f"{random.randint(0, 9)}",
+                                     f"{random.randint(0, 9)}"]
+                if self.logo_parts_duration_each <= self.intro_timer - self.intro_production_fade_sum_duration:
+                    self.logo_content[0] = "2"
+                    if 2 * self.logo_parts_duration_each <= self.intro_timer - self.intro_production_fade_sum_duration:
+                        self.logo_content[1] = "0"
+                    if 3 * self.logo_parts_duration_each <= self.intro_timer - self.intro_production_fade_sum_duration:
+                        self.logo_content[2] = "4"
+                    if 4 * self.logo_parts_duration_each <= self.intro_timer - self.intro_production_fade_sum_duration:
+                        self.logo_content[3] = "8"
+
+                self.logo_text.update_text("".join(self.logo_content))
+
+            elif 0 < self.intro_timer - self.intro_production_fade_sum_duration - self.logo_duration < self.logo_shrink_duration:
+                ds = (self.intro_timer - self.intro_production_fade_sum_duration - self.logo_duration) / self.logo_shrink_duration * self.logo_delta_font_size
+                dp = (self.intro_timer - self.intro_production_fade_sum_duration - self.logo_duration) / self.logo_shrink_duration * self.logo_delta_pos
+                self.logo_text.update_font_size(round(self.logo_starting_font_size - ds))
+                self.logo_text.update_position(self.logo_start_pos - dp)
+
+            elif 0 < self.intro_timer - self.intro_production_fade_sum_duration - self.logo_duration - self.logo_shrink_duration:
+                self.logo_text.update_font_size(self.logo_final_font_size)
+        elif self.state == "game":
             if self.grid.game_over():
                 print("Game Over")
                 self.state = "game_over"
